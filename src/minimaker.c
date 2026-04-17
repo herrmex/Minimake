4a6baa724e7a9948c1a13aa64af438e205c331aa
Hermex <marwan.yousef@epita.fr>
2026-04-17 08:52:40 +0200

    Adde main c file and its h file

---
+++ src/minimaker.c
@@ -0,0 +1,1 @@
+#include "minimaker.h"
---
+++ src/minimaker.h
@@ -0,0 +1,10 @@
+#ifndef MINIMAKER_H
+#define MINIMAKER_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#endif
